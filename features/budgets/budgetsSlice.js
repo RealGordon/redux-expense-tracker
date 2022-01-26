ad8aa02c0c12bfe61265c8f983fa8d38d859d832
RealGordon <92362350+RealGordon@users.noreply.github.com>
2022-01-26 22:40:53 +0000

    create repo

---
+++ features/budgets/budgetsSlice.js
@@ -0,0 +1,45 @@
+import {createSlice} from "@reduxjs/toolkit";
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
+
+
+const budgetsSlice=createSlice({
+  name:'budgets',
+  initialState,
+  reducers:{
+editBudget:(state,action)=>{
+const index=state.findIndex((budget)=>{
+  return budget.category===action.payload.category
+  })
+state[index]=action.payload
+}
+  }
+})
+/*
+export const editBudget = (budget) => {
+  return {
+    type: 'budgets/editBudget',
+    payload: budget
+  }
+}
+
+const budgetsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'budgets/editBudget':
+      const newBudgets = state.map(budget => {
+        if (budget.category === action.payload.category) {
+          return action.payload;
+        }
+        return budget;
+      })
+      return newBudgets;
+    default:
+      return state;
+  }
+}
+*/
+export const {editBudget}= budgetsSlice.actions;
+export const selectBudgets = (state) => state.budgets;
+
+//export default budgetsReducer;
+export default budgetsSlice.reducer;
